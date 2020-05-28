d2513177b94c3b7f6909781e429c23bf72ef6d8c
I. Arda Tuna <33693948+iardatuna@users.noreply.github.com>
2020-05-28 22:09:18 +0300

    Online Shopping Initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TunaETradeProject.Entity;
+
+namespace TunaETradeProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext _context = new DataContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(_context.Products.Where(i=>i.IsAtHomePage && i.IsApproved).ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(_context.Products.Where(i => i.Id == id).FirstOrDefault());
+        }
+
+        public ActionResult List()
+        {
+            return View(_context.Products.Where(i => i.IsApproved).ToList());
+        }
+
+
+
+    }
+}
